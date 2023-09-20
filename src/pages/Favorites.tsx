a98c644d310b23547b286df93fee0a7057ba8fc2
Rodrigo Bournissent <rodrigo@gramble.com>
2023-09-20 21:44:05 +0200

    Implement Favorites page
    Render favorite jokes

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,7 +1,26 @@
+import { useJokesContext } from "../context/JokesContext"
+import { Joke } from "../models/Joke"
+import { ActionTypes } from "../reducers/jokeReducer"
+
+import JokeCard from "../components/JokeCard"
+
 const Favorites = () => {
+  const { favorites, dispatch } = useJokesContext()
+
+  const toggleFavorite = (joke: Joke) => {
+    dispatch({ type: ActionTypes.FAVORITE, payload: joke })
+  }
+
   return (
-    <main className='mt-[40px] p-2'>
-      Favorites
+    <main className='mt-[40px] px-2 py-10'>
+      <section className="flex flex-col items-center gap-10 ">
+        {favorites.map(joke => (
+            <JokeCard key={joke.id}
+              joke={joke}
+              onToggleClicked={toggleFavorite} />
+          )
+        )}
+      </section>
     </main>
   )
 }
