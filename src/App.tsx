dab5497e695bb88dd30c5191e40c5bda3ee4a2f7
Rodrigo Bournissent <rodrigo@gramble.com>
2023-09-20 18:49:29 +0200

    Wrap components in App with JokesProvider
    So the children components have access to the context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from './components/Header'
 import AppRouter from './AppRouter'
+import JokesProvider from './context/JokesProvider'
 
 function App() {
   return (
-    <>
+    <JokesProvider>
       <Header />
       <AppRouter />
-    </>
+    </JokesProvider>
   )
 }
 
