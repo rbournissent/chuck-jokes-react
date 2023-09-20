5fa436e08d7f49f5ac674f59df8b6d251d742d9f
Rodrigo Bournissent <rodrigo@gramble.com>
2023-09-20 10:52:40 +0200

    Define router and renders it below Header
    Add react-router-dom as dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+
+import Header from './components/Header'
+import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/favorites', element: <Favorites /> },
+])
 
 function App() {
   return (
     <>
-      Chuck Jokes
+      <Header />
+      <RouterProvider router={router}/>
     </>
   )
 }
