091c05796bd64ecffe5b7dceb8d230db666b33a9
Rodrigo Bournissent <rodrigo@gramble.com>
2023-09-21 15:44:25 +0200

    Use heart icons in JokesCard

--- src/components/JokeCard.tsx
+++ src/components/JokeCard.tsx
@@ -1,22 +1,17 @@
 import { Joke } from "../models/Joke"
-
+import { IconHeartFilled, IconHeart } from '@tabler/icons-react'
 interface JokeProps {
   joke: Joke,
   onToggleClicked: (joke: Joke) => void
 }
 
 const JokeCard = ({ joke, onToggleClicked }: JokeProps) => {
-  let styles = 'absolute top-2 right-2 font-bold hover:text-red-600 transition-colors duration-200'
-
-  if (joke.isFavorite) {
-    styles += ' text-green-700'
-  }
-
   return (
     <article className="w-1/2 relative px-10 py-5 bg-slate-100 rounded-md shadow-md shadow-zinc-300">
       <p className="text-center italic">{ joke.text }</p>
-      <button className={styles} onClick={() => onToggleClicked(joke)}>
-        { '<3' }
+      <button className='absolute top-2 right-2 font-bold hover:text-red-600 transition-colors duration-200'
+         onClick={() => onToggleClicked(joke)}>
+        { joke.isFavorite ? <IconHeartFilled /> : <IconHeart /> }
       </button>
     </article>
   )
