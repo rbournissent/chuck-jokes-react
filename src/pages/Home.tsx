1df96dab4591f51a2395598844594169b9ab338f
Rodrigo Bournissent <rodrigo@gramble.com>
2023-09-21 11:46:52 +0200

    Countdown and toggle to pause / resume interval

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 import { Joke } from "../models/Joke"
 import { ActionTypes } from "../reducers/jokeReducer"
@@ -8,13 +8,35 @@ import JokeCard from "../components/JokeCard"
 import { useJokesContext } from "../context/JokesContext"
 
 const Home = () => {
-  const JOKE_INTERVAL_TIME = 5000
+  const JOKE_INTERVAL_TIME = 5 // seconds
   const { jokes, dispatch } = useJokesContext()
-  const [error, setError] = useState('')
+  const [error, setError] = useState<string>('')
+  const [intervalEnabled, setIntervalEnabled] = useState<boolean>(true)
+  const [countdown, setCountdown] = useState<number>(JOKE_INTERVAL_TIME)
 
   const toggleFavorite = (joke: Joke) => {
     dispatch({ type: ActionTypes.FAVORITE, payload: joke })
   }
+
+  const countdownInterval = useCallback(() => {
+    if (intervalEnabled) {
+      setCountdown(countdown - 1)
+    }
+  }, [intervalEnabled, countdown])
+
+  const toggleInterval = () => {
+    setIntervalEnabled(!intervalEnabled)
+  }
+
+  useEffect(() => {
+    if (!countdown) {
+      setCountdown(JOKE_INTERVAL_TIME)
+      getNewJoke().then(joke => {
+        dispatch({ type: ActionTypes.ADD, payload: joke })
+      })
+      .catch(() => setError('Failed to fetch a new joke'))
+    }
+  }, [dispatch, countdown])
 
   useEffect(() => {
     (() => {
@@ -30,23 +52,28 @@ const Home = () => {
 
   useEffect(() => {
     // Start interval
-    const id = window.setInterval(() => {
-      getNewJoke().then(joke => {
-        dispatch({ type: ActionTypes.ADD, payload: joke })
-      })
-      .catch(() => setError('Failed to fetch a new joke'))
-    }, JOKE_INTERVAL_TIME)
+    const intervalId = window.setInterval(countdownInterval, 1000)
 
     return () => {
-      window.clearInterval(id)
+      window.clearInterval(intervalId)
     }
-  }, [dispatch])
+  }, [countdownInterval])
 
 
   return (
     <main className='mt-[40px] px-2 py-10'>
       <section className="flex flex-col items-center gap-10 ">
         { error && <p className="text-red-600">{ error }</p> }
+        <p className="flex w-1/2 justify-between">
+          <i>{ intervalEnabled
+            ? `A new joke arrives in ${countdown} seconds...`
+            : 'Resume to get a new joke every 5 seconds'
+          }</i>
+          <button style={{ marginLeft: '10px' }}
+            onClick={toggleInterval}>
+            { intervalEnabled ? 'Pause' : 'Resume' }
+          </button>
+        </p>
         {jokes.map(joke => (
             <JokeCard key={joke.id}
               joke={joke}
