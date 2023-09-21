3107d5d84b201c1607410f3ee1a6143fde764c58
Rodrigo Bournissent <rodrigo@gramble.com>
2023-09-21 22:10:38 +0200

    Show placeholder if favorites list is empty

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -3,6 +3,7 @@ import { Joke } from "../models/Joke"
 import { ActionTypes } from "../reducers/jokeReducer"
 
 import JokeCard from "../components/JokeCard"
+import Card from "../components/Card"
 
 const Favorites = () => {
   const { favorites, dispatch } = useJokesContext()
@@ -14,6 +15,13 @@ const Favorites = () => {
   return (
     <main className='mt-[40px] px-2 py-10'>
       <section className="flex flex-col items-center gap-10 ">
+        { !favorites.length
+          ? <Card>
+            <p className="text-center">
+              { `You don't have any favorite jokes` }
+            </p>
+          </Card>
+          : '' }
         {favorites.map(joke => (
             <JokeCard key={joke.id}
               joke={joke}
